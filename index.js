a1006500055cb45d8600069e725836319e01557a
hari krishna <hari.jsdeveloper@gmail.com>
2023-05-02 17:55:55 +0530

    added partials and layouts

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const port = 8000;
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+//use express layouts
+app.use(expressLayouts);
+
+//extracting styles or scripts from sub pages to main page
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 //use the router
 app.use('/',require('./routes/index'));
 
+//setting the view engine and path
+app.set('view engine','ejs');
+app.set('views','./views');
 
 
 app.listen(port,function(err){
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const homeController = require('../controllers/home_controller');
 const router = express.Router();
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 console.log("router loaded");
 
 module.exports=router;
