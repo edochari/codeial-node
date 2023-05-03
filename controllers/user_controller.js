454afca2a31be1e328fa1fd90dd6a09f77412c9b
hari krishna <hari.jsdeveloper@gmail.com>
2023-05-03 14:06:09 +0530

    added sign in

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 
 module.exports.profile = function (req, res) {
-    return res.render('user', {
-        title: 'profile'
-    })
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id).then((user)=>{
+            if(user){
+                return res.render('user_profile', {
+                    title: ' User profile',
+                    user:user,
+                })
+            }else{
+                return res.redirect('back');
+            }
+        })
+    }else{
+        return res.redirect('back');
+    }
+    
 }
 
 module.exports.signUp = function (req, res) {
@@ -45,5 +57,15 @@ module.exports.create = function (req, res) {
 
 // sign in and create session
 module.exports.createSession = function (req, res) {
+   User.findOne( {email: req.body.email }).then((user)=>{
+    if(user.password !== req.body.password){
+        return res.redirect('back');
+    }
 
+    res.cookie('user_id',user.id);
+    return res.redirect('/users/profile');
+   })
+   .catch((err)=>{
+    console.log("Error in finding user");
+   })
 }
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,10 @@ const router = express.Router();
 router.get('/profile',userController.profile);
 router.get('/sign-up',userController.signUp);
 router.get('/sign-in',userController.signIn);
+
+
 router.post('/create',userController.create);
+router.post('/create-session',userController.createSession);
 
 
 module.exports=router;
